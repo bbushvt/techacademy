4e2d7a54b11a2e81f78f9592f0d5a24d4e2e4856
Bryan Bush <bbushvt@gmail.com>
2023-11-16 15:00:21 -0500

    increased timeout for image creation from default of 60 to 120

--- greece/main.tf
+++ greece/main.tf
@@ -11,11 +11,14 @@ resource "ibm_resource_instance" "pvs_workspace" {
 }
 
 resource "ibm_pi_image" "aix_image" {
-  pi_image_name     = var.image_name
-  pi_cloud_instance_id = ibm_resource_instance.pvs_workspace.guid
-  pi_image_bucket_name = var.bucket_name
-  pi_image_bucket_access = "public"
-  pi_image_bucket_region = var.bucket_region
+  pi_image_name             = var.image_name
+  pi_cloud_instance_id      = ibm_resource_instance.pvs_workspace.guid
+  pi_image_bucket_name      = var.bucket_name
+  pi_image_bucket_access    = "public"
+  pi_image_bucket_region    = var.bucket_region
   pi_image_bucket_file_name = var.bucket_file_name
-  pi_image_storage_type = var.storage_type
+  pi_image_storage_type     = var.storage_type
+  timeouts {
+    create = "120m"
+  }
 }
