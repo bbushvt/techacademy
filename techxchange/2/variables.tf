256c96e8b2e8fab00b35c92725fb01753d5d7d93
Bryan Bush <bbushvt@gmail.com>
2025-04-27 21:06:31 -0500

    Adding an incremental change to a different directory

---
+++ techxchange/2/variables.tf
@@ -0,0 +1,9 @@
+variable "pvs_region_a" {
+  description = "Where to deploy the Power VS Workspace"
+  default = "tor01"
+}
+
+variable "workspace_name_a" {
+  description = "The name of the workspace to create"
+  default = "bcbush_pvs_workspace_tor01"
+}
