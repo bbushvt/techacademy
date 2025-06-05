b7742baf00b5ba861b89dbe7a5baf8b8a5f7683c
Bryan Bush <bbushvt@gmail.com>
2025-06-05 14:16:58 -0400

    changed id to guid

--- mark_o_techxchange_2025/main.tf
+++ mark_o_techxchange_2025/main.tf
@@ -14,7 +14,7 @@ resource "ibm_resource_instance" "pvs_workspace_a" {
 resource "ibm_pi_network" "pvs_network_workspace_a" {
   count                = 1
   pi_network_name      = "main"
-  pi_cloud_instance_id = ibm_resource_instance.pvs_workspace_a.id
+  pi_cloud_instance_id = ibm_resource_instance.pvs_workspace_a.guid
   pi_network_type      = "vlan"
   pi_cidr              = "192.168.0.0/24"
   pi_dns               = ["8.8.8.8"]
@@ -28,7 +28,7 @@ resource "ibm_pi_network" "pvs_network_workspace_a" {
 
 resource "ibm_pi_image" "aix73"{
   pi_image_id          = "91e60650-3aa2-4d1c-afee-e649b53e170a"
-  pi_cloud_instance_id = ibm_resource_instance.pvs_workspace_a.id
+  pi_cloud_instance_id = ibm_resource_instance.pvs_workspace_a.guid
   provider             = ibm.a
 }
 
