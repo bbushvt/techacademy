6b31b5b991233292ab65ad54caebd328d04a24f0
Bryan Bush <bbushvt@gmail.com>
2025-07-02 11:10:34 -0400

    Added Tech Session for the 2Q QBR

---
+++ qbr_tech_session_2025/main.tf
@@ -0,0 +1,173 @@
+data "ibm_resource_group" "group" {
+  name = "Default"
+}
+########################################################
+# Workspace in data center A
+########################################################
+
+#  Create the workspace in the A location
+resource "ibm_resource_instance" "pvs_workspace_a" {
+  name              = var.workspace_name_a
+  service           = "power-iaas"
+  location          = var.pvs_region_a
+  plan              = "power-virtual-server-group"
+  resource_group_id = data.ibm_resource_group.group.id
+  provider          = ibm.a
+}
+
+# Create a network in the A workspace
+resource "ibm_pi_network" "pvs_network_workspace_a" {
+  pi_network_name      = "main"
+  pi_cloud_instance_id = ibm_resource_instance.pvs_workspace_a.guid
+  pi_network_type      = "vlan"
+  pi_cidr              = "192.168.0.0/24"
+  pi_dns               = ["8.8.8.8"]
+  pi_gateway           = "192.168.0.1"
+  pi_ipaddress_range {
+    pi_starting_ip_address  = "192.168.0.2"
+    pi_ending_ip_address    = "192.168.0.254"
+  }
+  provider             = ibm.a
+}
+
+# Create an SSH key 
+resource  "ibm_pi_key" "ssh_key_a" {
+  pi_key_name          = "qbr_ssh_key"
+  pi_cloud_instance_id = ibm_resource_instance.pvs_workspace_a.guid
+  pi_ssh_key           = var.ssh_key_rsa
+  provider             = ibm.a
+}
+
+# Create an instance in workspace A
+resource "ibm_pi_instance" "test-instance" {
+    pi_memory             = "4"
+    pi_processors         = "2"
+    pi_instance_name      = var.powervs_workspace_a_instance_name
+    pi_proc_type          = "shared"
+    pi_image_id           = "91414a26-212a-4780-83cf-330f192f2225"
+    pi_key_pair_name      = ibm_pi_key.ssh_key_a.name
+    pi_sys_type           = "s1022"
+    pi_cloud_instance_id  = ibm_resource_instance.pvs_workspace_a.guid
+    pi_pin_policy         = "none"
+    pi_health_status      = "WARNING"
+    pi_network {
+      network_id          = ibm_pi_network.pvs_network_workspace_a.network_id
+      ip_address          = "192.168.0.10"
+    }
+    provider              = ibm.a
+}
+
+########################################################
+# VPC in us-east-1
+########################################################
+
+# Create the VPC
+resource "ibm_is_vpc" "admin_vpc" {
+  name                        = var.vpc_name
+  resource_group              = data.ibm_resource_group.group.id
+  address_prefix_management   = "manual"
+  provider                    = ibm.vpc
+}
+
+# Create a prefix in the VPC
+resource "ibm_is_vpc_address_prefix" "test_vpc_test_vpc_zone_1_prefix" {
+  name = "${var.vpc_name}-test-vpc-test-vpc-zone-1"
+  vpc  = ibm_is_vpc.admin_vpc.id
+  zone = "${var.vpc_region}-1"
+  cidr = "192.168.2.0/24"
+  provider = ibm.vpc
+}
+
+# Create the network ACL
+resource "ibm_is_network_acl" "test_vpc_main_acl_acl" {
+  name           = "test-vpc-main-acl-acl"
+  vpc            = ibm_is_vpc.admin_vpc.id
+  resource_group = data.ibm_resource_group.group.id
+  provider = ibm.vpc
+  rules {
+    action      = "allow"
+    destination = "0.0.0.0/0"
+    direction   = "inbound"
+    name        = "inbound"
+    source      = "0.0.0.0/0"
+  }
+  rules {
+    action      = "allow"
+    destination = "0.0.0.0/0"
+    direction   = "outbound"
+    name        = "outbound"
+    source      = "0.0.0.0/0"
+  }
+}
+
+# Create the SSH key in the vpc
+resource "ibm_is_ssh_key" "vpc_ssh_key" {
+  name          = var.ssh_key_name
+  public_key    = var.ssh_key_rsa
+  type          = "rsa"
+  provider      = ibm.vpc
+}
+
+# Create a subnet from the prefix and using the ACL we created
+resource "ibm_is_subnet" "test_vpc_main_zone_1" {
+  vpc             = ibm_is_vpc.admin_vpc.id
+  name            = "test-vpc-main-zone-1"
+  zone            = "${var.vpc_region}-1"
+  resource_group  = data.ibm_resource_group.group.id
+  network_acl     = ibm_is_network_acl.test_vpc_main_acl_acl.id
+  ipv4_cidr_block = "192.168.2.0/24"
+  tags = []
+  depends_on = [
+    ibm_is_vpc_address_prefix.test_vpc_test_vpc_zone_1_prefix
+  ]
+  provider = ibm.vpc
+}
+
+# Create a VSI
+resource "ibm_is_instance" "instance1" {
+  name                = var.vsi_instance_name
+  image               = var.vpc_image_id
+  profile             = var.vsi_profile
+  primary_network_interface {
+    subnet            = ibm_is_subnet.test_vpc_main_zone_1.id
+  }
+  vpc                 = ibm_is_vpc.admin_vpc.id
+  zone                = var.vpc_zone
+  keys                = [ibm_is_ssh_key.vpc_ssh_key.id]
+  resource_group      = data.ibm_resource_group.group.id
+  provider            = ibm.vpc
+}
+
+# Create the FIP for the VSI
+resource "ibm_is_floating_ip" "vis_fip" {
+  name                = "vsi-fip"
+  target              = ibm_is_instance.instance1.primary_network_interface[0].id
+  provider            = ibm.vpc
+}
+
+# Create the Transit Gateway
+resource "ibm_tg_gateway" "main_tgw" {
+  name                = "main_tgw"
+  location            = var.vpc_region
+  resource_group      = data.ibm_resource_group.group.id
+  global              = true
+  provider            = ibm.vpc
+}
+
+# Connection for PowerVS Workspace A
+resource "ibm_tg_connection" "pvs_workspace_a" {
+  gateway             = ibm_tg_gateway.main_tgw.id
+  name                = "powervs_workspace_a"
+  network_type        = "power_virtual_server"
+  network_id          = ibm_resource_instance.pvs_workspace_a.id
+  provider            = ibm.vpc
+}
+
+# Connection for VPC
+resource "ibm_tg_connection" "vpc" {
+  gateway             = ibm_tg_gateway.main_tgw.id
+  name                = "vpc"
+  network_type        = "vpc"
+  network_id          = ibm_is_vpc.admin_vpc.crn
+  provider            = ibm.vpc
+}
---
+++ qbr_tech_session_2025/variables.tf
@@ -0,0 +1,64 @@
+variable "pvs_region_a" {
+  description = "Where to deploy the Power VS Workspace A"
+  default = "wdc06"
+}
+
+variable "workspace_name_a" {
+  description = "The name of the workspace to create in region a"
+  default = "Production"
+}
+
+variable "powervs_workspace_a_instance_name" {
+  default = "aix_prod_instance"
+}
+
+variable "storage_type_a" {
+  description = "This is the Power VS storage type to use for the server deployed in workspace a"
+  default = "tier3"
+}
+
+variable "ssh_key_name" {
+  description = "Name of the ssh key to be used"
+  type        = string
+  default     = "mysshkey"
+}
+
+variable "ssh_key_rsa" {
+  description = "Public ssh key"
+  type        = string
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDK0BHNL2ct5XpfQtY4vMEfON7ac2H3+X6p885vFEPSKyJwLCqflrRE2bi+R7YBRwM54VZKFQ+K9t3GjZXnRmZ7Ob+c9E5viE778X9bZMx7Yk3yZ8/j/peJpLYrw8ya+PqlExhNc4IBGBkSdhyRZ0f52SyreYhMA5wYspYyN+qSlp1PrDZl5ih5USzlXmfUebaG4r3/ADORUOkHuIUlopBr+mxE4D/cMS+I5en4Epzi8u0VdR7IjOZ4YzsqJYIDKtGsg7cyif/vvQct9F1GpLsUyXnrS/iL90cDgfSvUuI9dkMfRD+hsLIRLMMUDrLHSBbpeJJ1SHuQpfVOmnzcyEIHTLfqd1W+EslKFpThkeQ4I7gn+N4CD7ZkdqYp65IWy7fkkQaHjVhFOMNrV8epje2aY80pQc1/6BsQwzUn9rewV+MQnMfG96gtL29/osE2QWoAtO2WEf3rONBmllqLugJZqd0JHd3CgjIkPKFg6aHPzZp8711GEKIdLhaOCni7j7y5u8Pqv/M7sDl9nlg8UZ9CMRWuINM39lpEu0S9PF1OLNca3ECC4A6cfi+BcEjzpGbi/AtzkelfIeB5aGPZllclikI9UDel2s+MRGNipWDGTzBnPr1PVzHFPmEnT3GbUUzzGCttTQS1hAAdIsnr1Ql8W1tX6otofe/7T2D0fZgpxw=="
+}
+
+variable "vpc_region" {
+  description = "IBM Cloud Region where resources will be provisioned"
+  type        = string
+  default     = "us-east"
+  validation {
+    error_message = "Region must be in a supported IBM VPC region."
+    condition     = contains(["us-south", "us-east", "br-sao", "ca-tor", "eu-gb", "eu-de", "jp-tok", "jp-osa", "au-syd"], var.vpc_region)
+  }
+}
+
+variable "vpc_name" {
+  description = "The name of the VPC"
+  type = string
+  default = "myvpc"
+}
+
+variable "vpc_image_id" {
+  default = "r014-2dfb4fd7-2af0-4df0-a8e1-97eaa04b6156"
+  description = "The image id used for the VSI in the VPC"
+}
+
+variable "vsi_profile" {
+  default = "cx2-2x4"
+  description = "The default profile to use, a cx2-2x4"
+}
+
+variable "vpc_zone" {
+  default = "us-east-1"  
+}
+
+variable "vsi_instance_name" {
+  default = "jumpserver"
+}
