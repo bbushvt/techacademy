0d18d788d7524bddca15c76457ddac85297fe27d
Bryan Bush <bbushvt@gmail.com>
2025-07-09 15:03:10 -0400

    commenting out everything other than powervs to test the ssh key part of the deployment

--- qbr_tech_session_2025/main.tf
+++ qbr_tech_session_2025/main.tf
@@ -64,120 +64,120 @@ resource "ibm_pi_instance" "test-instance" {
     provider              = ibm.a
 }
 
-########################################################
-# VPC in us-east-1
-########################################################
+# ########################################################
+# # VPC in us-east-1
+# ########################################################
 
-# Create the VPC
-resource "ibm_is_vpc" "admin_vpc" {
-  name                        = var.vpc_name
-  resource_group              = data.ibm_resource_group.group.id
-  address_prefix_management   = "manual"
-  provider                    = ibm.vpc
-}
-
-# Create a prefix in the VPC
-resource "ibm_is_vpc_address_prefix" "test_vpc_test_vpc_zone_1_prefix" {
-  name = "${var.vpc_name}-test-vpc-test-vpc-zone-1"
-  vpc  = ibm_is_vpc.admin_vpc.id
-  zone = "${var.vpc_region}-1"
-  cidr = "192.168.2.0/24"
-  provider = ibm.vpc
-}
-
-# Create the network ACL
-resource "ibm_is_network_acl" "test_vpc_main_acl_acl" {
-  name           = "test-vpc-main-acl-acl"
-  vpc            = ibm_is_vpc.admin_vpc.id
-  resource_group = data.ibm_resource_group.group.id
-  provider = ibm.vpc
-  rules {
-    action      = "allow"
-    destination = "0.0.0.0/0"
-    direction   = "inbound"
-    name        = "inbound"
-    source      = "0.0.0.0/0"
-  }
-  rules {
-    action      = "allow"
-    destination = "0.0.0.0/0"
-    direction   = "outbound"
-    name        = "outbound"
-    source      = "0.0.0.0/0"
-  }
-}
-
-# # Create the SSH key in the vpc
-# resource "ibm_is_ssh_key" "vpc_ssh_key" {
-#   name          = var.ssh_key_name
-#   public_key    = var.ssh_key_rsa
-#   type          = "rsa"
-#   provider      = ibm.vpc
+# # Create the VPC
+# resource "ibm_is_vpc" "admin_vpc" {
+#   name                        = var.vpc_name
+#   resource_group              = data.ibm_resource_group.group.id
+#   address_prefix_management   = "manual"
+#   provider                    = ibm.vpc
 # }
 
-# Create a subnet from the prefix and using the ACL we created
-resource "ibm_is_subnet" "test_vpc_main_zone_1" {
-  vpc             = ibm_is_vpc.admin_vpc.id
-  name            = "test-vpc-main-zone-1"
-  zone            = "${var.vpc_region}-1"
-  resource_group  = data.ibm_resource_group.group.id
-  network_acl     = ibm_is_network_acl.test_vpc_main_acl_acl.id
-  ipv4_cidr_block = "192.168.2.0/24"
-  tags = []
-  depends_on = [
-    ibm_is_vpc_address_prefix.test_vpc_test_vpc_zone_1_prefix
-  ]
-  provider = ibm.vpc
-}
+# # Create a prefix in the VPC
+# resource "ibm_is_vpc_address_prefix" "test_vpc_test_vpc_zone_1_prefix" {
+#   name = "${var.vpc_name}-test-vpc-test-vpc-zone-1"
+#   vpc  = ibm_is_vpc.admin_vpc.id
+#   zone = "${var.vpc_region}-1"
+#   cidr = "192.168.2.0/24"
+#   provider = ibm.vpc
+# }
 
-# Create a VSI
-resource "ibm_is_instance" "instance1" {
-  name                = var.vsi_instance_name
-  image               = var.vpc_image_id
-  profile             = var.vsi_profile
-  primary_network_interface {
-    subnet            = ibm_is_subnet.test_vpc_main_zone_1.id
-  }
-  vpc                 = ibm_is_vpc.admin_vpc.id
-  zone                = var.vpc_zone
-  keys                = [data.ibm_is_ssh_key.bcbush_1pass.id]
-  resource_group      = data.ibm_resource_group.group.id
-  provider            = ibm.vpc
-}
+# # Create the network ACL
+# resource "ibm_is_network_acl" "test_vpc_main_acl_acl" {
+#   name           = "test-vpc-main-acl-acl"
+#   vpc            = ibm_is_vpc.admin_vpc.id
+#   resource_group = data.ibm_resource_group.group.id
+#   provider = ibm.vpc
+#   rules {
+#     action      = "allow"
+#     destination = "0.0.0.0/0"
+#     direction   = "inbound"
+#     name        = "inbound"
+#     source      = "0.0.0.0/0"
+#   }
+#   rules {
+#     action      = "allow"
+#     destination = "0.0.0.0/0"
+#     direction   = "outbound"
+#     name        = "outbound"
+#     source      = "0.0.0.0/0"
+#   }
+# }
 
-# Create the FIP for the VSI
-resource "ibm_is_floating_ip" "vis_fip" {
-  name                = "vsi-fip"
-  target              = ibm_is_instance.instance1.primary_network_interface[0].id
-  provider            = ibm.vpc
-}
+# # # Create the SSH key in the vpc
+# # resource "ibm_is_ssh_key" "vpc_ssh_key" {
+# #   name          = var.ssh_key_name
+# #   public_key    = var.ssh_key_rsa
+# #   type          = "rsa"
+# #   provider      = ibm.vpc
+# # }
 
-# Create the Transit Gateway
-resource "ibm_tg_gateway" "main_tgw" {
-  name                = "main_tgw"
-  location            = var.vpc_region
-  resource_group      = data.ibm_resource_group.group.id
-  global              = false
-  provider            = ibm.vpc
-}
+# # Create a subnet from the prefix and using the ACL we created
+# resource "ibm_is_subnet" "test_vpc_main_zone_1" {
+#   vpc             = ibm_is_vpc.admin_vpc.id
+#   name            = "test-vpc-main-zone-1"
+#   zone            = "${var.vpc_region}-1"
+#   resource_group  = data.ibm_resource_group.group.id
+#   network_acl     = ibm_is_network_acl.test_vpc_main_acl_acl.id
+#   ipv4_cidr_block = "192.168.2.0/24"
+#   tags = []
+#   depends_on = [
+#     ibm_is_vpc_address_prefix.test_vpc_test_vpc_zone_1_prefix
+#   ]
+#   provider = ibm.vpc
+# }
 
-# Connection for PowerVS Workspace A
-resource "ibm_tg_connection" "pvs_workspace_a" {
-  gateway             = ibm_tg_gateway.main_tgw.id
-  name                = "powervs_workspace_a"
-  network_type        = "power_virtual_server"
-  network_id          = ibm_resource_instance.pvs_workspace_a.id
-  provider            = ibm.vpc
-}
+# # Create a VSI
+# resource "ibm_is_instance" "instance1" {
+#   name                = var.vsi_instance_name
+#   image               = var.vpc_image_id
+#   profile             = var.vsi_profile
+#   primary_network_interface {
+#     subnet            = ibm_is_subnet.test_vpc_main_zone_1.id
+#   }
+#   vpc                 = ibm_is_vpc.admin_vpc.id
+#   zone                = var.vpc_zone
+#   keys                = [data.ibm_is_ssh_key.bcbush_1pass.id]
+#   resource_group      = data.ibm_resource_group.group.id
+#   provider            = ibm.vpc
+# }
 
-# Connection for VPC
-resource "ibm_tg_connection" "vpc" {
-  gateway             = ibm_tg_gateway.main_tgw.id
-  name                = "vpc"
-  network_type        = "vpc"
-  network_id          = ibm_is_vpc.admin_vpc.crn
-  provider            = ibm.vpc
-}
+# # Create the FIP for the VSI
+# resource "ibm_is_floating_ip" "vis_fip" {
+#   name                = "vsi-fip"
+#   target              = ibm_is_instance.instance1.primary_network_interface[0].id
+#   provider            = ibm.vpc
+# }
+
+# # Create the Transit Gateway
+# resource "ibm_tg_gateway" "main_tgw" {
+#   name                = "main_tgw"
+#   location            = var.vpc_region
+#   resource_group      = data.ibm_resource_group.group.id
+#   global              = false
+#   provider            = ibm.vpc
+# }
+
+# # Connection for PowerVS Workspace A
+# resource "ibm_tg_connection" "pvs_workspace_a" {
+#   gateway             = ibm_tg_gateway.main_tgw.id
+#   name                = "powervs_workspace_a"
+#   network_type        = "power_virtual_server"
+#   network_id          = ibm_resource_instance.pvs_workspace_a.id
+#   provider            = ibm.vpc
+# }
+
+# # Connection for VPC
+# resource "ibm_tg_connection" "vpc" {
+#   gateway             = ibm_tg_gateway.main_tgw.id
+#   name                = "vpc"
+#   network_type        = "vpc"
+#   network_id          = ibm_is_vpc.admin_vpc.crn
+#   provider            = ibm.vpc
+# }
 
 # # Create the Compass by Cobalt Iron Virtual Private Endpoint Gateway
 # resource "ibm_is_virtual_endpoint_gateway" "compass-us-east" {
