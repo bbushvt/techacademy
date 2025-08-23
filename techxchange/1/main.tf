2121b4b23e5e5eb9d240526cc7d385c18d0e4fdf
Bryan Bush <bbushvt@gmail.com>
2025-08-22 23:04:12 -0400

    Added variables and main terraform code to create a subnet, ssh key, and deploy aix lpar

--- techxchange/1/main.tf
+++ techxchange/1/main.tf
@@ -11,3 +11,39 @@ resource "ibm_resource_instance" "pvs_workspace_a" {
 }
 
 
+# Create a network in the workspace a
+resource "ibm_pi_network" "pvs_workspace_a_network" {
+  pi_network_name      = var.workspace_a_subnet_name
+  pi_cloud_instance_id = ibm_resource_instance.pvs_workspace_a.guid
+  pi_network_type      = "vlan"
+  pi_cidr              = var.workspace_a_subnet
+  provider             = ibm.pvs_a
+}
+
+# Create an SSH key 
+resource  "ibm_pi_key" "ssh_key_a" {
+  pi_key_name          = var.ssh_key_name
+  pi_cloud_instance_id = ibm_resource_instance.pvs_workspace_a.guid
+  pi_ssh_key           = var.ssh_public_key
+  provider             = ibm.pvs_a
+}
+
+# Create an instance in workspace A
+resource "ibm_pi_instance" "pvs_workspace_a_aix_lpar" {
+    pi_memory             = "4"
+    pi_processors         = "2"
+    pi_instance_name      = var.workspace_a_instance_name
+    pi_proc_type          = "shared"
+    pi_image_id           = var.workspace_a_aix_image_id
+    pi_key_pair_name      = ibm_pi_key.ssh_key_a.name
+    pi_sys_type           = var.workspace_a_system_type
+    pi_cloud_instance_id  = ibm_resource_instance.pvs_workspace_a.guid
+    pi_pin_policy         = "none"
+    pi_health_status      = "WARNING"
+    pi_network {
+      network_id          = ibm_pi_network.pvs_workspace_a_network.network_id
+      ip_address          = var.workspace_a_system_ip_address
+    }
+    provider              = ibm.pvs_a
+    depends_on = [ ibm_pi_key.ssh_key_a ]
+}
--- techxchange/1/variables.tf
+++ techxchange/1/variables.tf
@@ -9,21 +9,41 @@ variable "workspace_a_name" {
 }
 
 variable "workspace_a_subnet_name" {
-  description = "This wil be the name of the subnet in workspace a"
+  description = "The name of the subnet in workspace a"
   default = "main"
 }
 
 variable "workspace_a_subnet" {
-  description = "This is the CIDR for the subnet that will be created in workspace a"
+  description = "The CIDR for the subnet that will be created in workspace a"
   default = "192.168.0.0/24"
 }
 
 variable "ssh_public_key" {
-  description = "This is the public SSH key to use in IBM Cloud"
+  description = "The public SSH key to use in IBM Cloud"
   default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCVcyJ8A3WvOAwp/k5NjgUvXXkDsfWxDiqhLEwv3X+l8u83qK0gtNcFD8RrabRGymFqRgYNTgFihXtIjAu7g/hSD+JEq/U8UE3fy6iWJ5yqPwG5PHnOYuKWMA205wEct6Cwo/vmG71OpVoezDJY8sJbdfTCjoQqir3ciiq73rlCPad6jJ0DslVwojhT0Q9PEP+ukslZgbf8nckkuKonb1ShCJBlWbXz9QQxYvw9wAh+MmFCBY6ktZyWskOM4M1DnBWVttecCUlq3OK3CsyahoszfA4UQDM92TUYrj2gF0ocQ/klGIMEJEvuFarI+p7CzO4tEMrxeDx3hkoSH4mtfjHA1d3hSBdvV2hgE2jp1hOXLIa7K1WhOFcb3qLDkWgIPwuPO93SeRRHcFaSj2MQx89rgDZUzZPa6i/ul7CIgoagp2eJRK9A4lCScl+usRi2ps6FRos8yGDnzBxXdJz+rt4zhYcOMtUQDL8MPH+BC/3xaRrlz6aY92EOJ5vSeLOqtP0="
 }
 
 variable "ssh_key_name" {
-  description = "This will be the name used for the public ssh key"
+  description = "The name for the public ssh key"
   default = "techxchange-ssh-key"
+}
+
+variable "workspace_a_instance_name" {
+  description = "The name of the AIX instance"
+  default = "techxchange_aix_vsi"
+}
+
+variable "workspace_a_aix_image_id" {
+  description = "The image ID for the AIX instance"
+  default = "7337579b-cc50-46ea-81d8-7cd06788dcbf"
+}
+
+variable "workspace_a_system_type" {
+  description = "The type of system to deploy (S922/S1022/S1122)"
+  default = "S1022"
+}
+
+variable "workspace_a_system_ip_address" {
+  description = "The IP address to use for the AIX lpar (needs to be in the subnet being created)"
+  default = "192.168.0.10"
 }
