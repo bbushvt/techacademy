88d1fa8628a88e6a5e0ededda4b9bf3035d148d8
Bryan Bush <bbushvt@gmail.com>
2025-08-23 09:56:57 -0400

    Added code to setup the management vpc

--- techxchange/2/main.tf
+++ techxchange/2/main.tf
@@ -1,27 +1,138 @@
 data "ibm_resource_group" "default" {
   name = "Default"
 }
-
-data "ibm_resource_group" "cloud_techsales" {
-  name = "cloud-techsales"
-}
-
 resource "ibm_resource_instance" "pvs_workspace_a" {
-  name              = var.workspace_name_a
+  name              = var.workspace_a_name
   service           = "power-iaas"
-  location          = var.pvs_region_a
+  location          = var.pvs_data_center
   plan              = "power-virtual-server-group"
-  resource_group_id = data.ibm_resource_group.cloud_techsales.id
-  provider          = ibm.a
+  resource_group_id = data.ibm_resource_group.default.id
+  provider          = ibm.pvs_a
 }
 
-resource "ibm_pi_network" "power_networks" {
-  count                = 1
-  pi_network_name      = "main"
+# Create a network in the workspace a
+resource "ibm_pi_network" "pvs_workspace_a_network" {
+  pi_network_name      = var.workspace_a_subnet_name
   pi_cloud_instance_id = ibm_resource_instance.pvs_workspace_a.guid
   pi_network_type      = "vlan"
-  pi_cidr              = "172.16.0.0/24"
-  provider             = ibm.a
+  pi_cidr              = var.workspace_a_subnet
+  provider             = ibm.pvs_a
 }
 
+# Create an SSH key 
+resource  "ibm_pi_key" "ssh_key_a" {
+  pi_key_name          = var.ssh_key_name
+  pi_cloud_instance_id = ibm_resource_instance.pvs_workspace_a.guid
+  pi_ssh_key           = var.ssh_public_key
+  provider             = ibm.pvs_a
+}
 
+# Create an instance in workspace A
+resource "ibm_pi_instance" "pvs_workspace_a_aix_lpar" {
+    pi_memory             = "4"
+    pi_processors         = "2"
+    pi_instance_name      = var.workspace_a_instance_name
+    pi_proc_type          = "shared"
+    pi_image_id           = var.workspace_a_aix_image_id
+    pi_key_pair_name      = ibm_pi_key.ssh_key_a.name
+    pi_sys_type           = var.workspace_a_system_type
+    pi_cloud_instance_id  = ibm_resource_instance.pvs_workspace_a.guid
+    pi_pin_policy         = "none"
+    pi_health_status      = "WARNING"
+    pi_storage_type       = var.workspace_a_storage_tier
+    pi_network {
+      network_id          = ibm_pi_network.pvs_workspace_a_network.network_id
+      ip_address          = var.workspace_a_system_ip_address
+    }
+    provider              = ibm.pvs_a
+    depends_on = [ ibm_pi_key.ssh_key_a ]
+}
+
+# Create the VPC
+resource "ibm_is_vpc" "mgmt_vpc" {
+  name                        = var.mgmt_vpc_name
+  resource_group              = data.ibm_resource_group.default.id
+  address_prefix_management   = "manual"
+  provider                    = ibm.mgmt_vpc
+}
+
+# Create a prefix in the VPC
+resource "ibm_is_vpc_address_prefix" "mgmt_vpc_prefix" {
+  name = "${var.mgmt_vpc_name}-${var.mgmt_vpc_zone}-prefix"
+  vpc  = ibm_is_vpc.mgmt_vpc.id
+  zone = var.mgmt_vpc_zone
+  cidr = var.vpc_mgmt_prefix
+  provider = ibm.mgmt_vpc
+}
+
+# Create the network ACL for the management vpc
+resource "ibm_is_network_acl" "mgmt_vpc_main_acl" {
+  name           = "mgmt_vpc_main_acl"
+  vpc            = ibm_is_vpc.mgmt_vpc.id
+  resource_group = data.ibm_resource_group.default.id
+  rules {
+    action      = "allow"
+    destination = "0.0.0.0/0"
+    direction   = "inbound"
+    name        = "inbound"
+    source      = "0.0.0.0/0"
+  }
+  rules {
+    action      = "allow"
+    destination = "0.0.0.0/0"
+    direction   = "outbound"
+    name        = "outbound"
+    source      = "0.0.0.0/0"
+  }
+  provider = ibm.mgmt_vpc
+}
+
+# Create a subnet from the prefix and using the ACL we created
+resource "ibm_is_subnet" "mgmt_vpc_sn" {
+  vpc             = ibm_is_vpc.mgmt_vpc.id
+  name            = "${var.mgmt_vpc_name}-${var.mgmt_vpc_zone}-sn"
+  zone            = var.mgmt_vpc_zone
+  resource_group  = data.ibm_resource_group.default.id
+  network_acl     = ibm_is_network_acl.mgmt_vpc_main_acl.id
+  ipv4_cidr_block = var.vpc_mgmt_sn
+  tags = []
+  depends_on = [
+    ibm_is_vpc_address_prefix.mgmt_vpc_prefix
+  ]
+  provider = ibm.mgmt_vpc
+}
+
+# add SSH rule to defualt security group
+resource "ibm_is_security_group_rule" "sg_ssh_rule" {
+  group     = ibm_is_vpc.mgmt_vpc.default_security_group
+  direction = "inbound"
+  remote    = "0.0.0.0/0"
+  tcp {
+    port_min = "22"
+    port_max = "22"
+  }
+}
+
+# Create the SSH key in the vpc
+resource "ibm_is_ssh_key" "jumpserver_ssh_key" {
+  name          = var.ssh_key_name
+  public_key    = var.ssh_public_key
+  type          = "rsa"
+  provider      = ibm.mgmt_vpc
+}
+
+# Create jumpserver VSI in the management VPC
+resource "ibm_is_instance" "jumpserver" {
+  name                = var.jumpserver_name
+  image               = var.jumpserver_image_id
+  profile             = var.jumpserver_profile
+  primary_network_interface {
+    subnet            = ibm_is_subnet.mgmt_vpc_sn.id
+  }
+  vpc                 = ibm_is_vpc.mgmt_vpc.id
+  zone                = var.mgmt_vpc_zone
+  keys                = [ibm_is_ssh_key.jumpserver_ssh_key.id]
+  resource_group      = data.ibm_resource_group.default.id
+  provider            = ibm.mgmt_vpc
+
+}
--- techxchange/2/variables.tf
+++ techxchange/2/variables.tf
@@ -1,9 +1,99 @@
-variable "pvs_region_a" {
+variable "pvs_data_center" {
   description = "Where to deploy the Power VS Workspace"
-  default = "tor01"
+  default = "wdc07"
 }
 
-variable "workspace_name_a" {
+variable "workspace_a_name" {
   description = "The name of the workspace to create"
-  default = "bcbush_pvs_workspace_tor01"
+  default = "techxchange-pvs-workspace"
+}
+
+variable "workspace_a_subnet_name" {
+  description = "The name of the subnet in workspace a"
+  default = "main"
+}
+
+variable "workspace_a_subnet" {
+  description = "The CIDR for the subnet that will be created in workspace a"
+  default = "192.168.0.0/24"
+}
+
+variable "ssh_public_key" {
+  description = "The public SSH key to use in IBM Cloud"
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCVcyJ8A3WvOAwp/k5NjgUvXXkDsfWxDiqhLEwv3X+l8u83qK0gtNcFD8RrabRGymFqRgYNTgFihXtIjAu7g/hSD+JEq/U8UE3fy6iWJ5yqPwG5PHnOYuKWMA205wEct6Cwo/vmG71OpVoezDJY8sJbdfTCjoQqir3ciiq73rlCPad6jJ0DslVwojhT0Q9PEP+ukslZgbf8nckkuKonb1ShCJBlWbXz9QQxYvw9wAh+MmFCBY6ktZyWskOM4M1DnBWVttecCUlq3OK3CsyahoszfA4UQDM92TUYrj2gF0ocQ/klGIMEJEvuFarI+p7CzO4tEMrxeDx3hkoSH4mtfjHA1d3hSBdvV2hgE2jp1hOXLIa7K1WhOFcb3qLDkWgIPwuPO93SeRRHcFaSj2MQx89rgDZUzZPa6i/ul7CIgoagp2eJRK9A4lCScl+usRi2ps6FRos8yGDnzBxXdJz+rt4zhYcOMtUQDL8MPH+BC/3xaRrlz6aY92EOJ5vSeLOqtP0="
+}
+
+variable "ssh_key_name" {
+  description = "The name for the public ssh key"
+  default = "techxchange-ssh-key"
+}
+
+variable "workspace_a_instance_name" {
+  description = "The name of the AIX instance"
+  default = "techxchange_aix_vsi"
+}
+
+variable "workspace_a_aix_image_id" {
+  description = "The image ID for the AIX instance"
+  default = "7337579b-cc50-46ea-81d8-7cd06788dcbf"
+}
+
+variable "workspace_a_system_type" {
+  description = "The type of system to deploy (s922/s1022/s1122)"
+  default = "s1022"
+}
+
+variable "workspace_a_system_ip_address" {
+  description = "The IP address to use for the AIX lpar (needs to be in the subnet being created)"
+  default = "192.168.0.10"
+}
+
+variable "workspace_a_storage_tier" {
+  description = "The storage tier to use for the disk on the LPAR"
+  default = "tier5k"
+}
+
+variable "mgmt_vpc_name" {
+  description = "The name of the management vpc"
+  default = "management-vpc"
+}
+
+variable "mgmt_vpc_region" {
+  description = "IBM Cloud Region where the VPC will be provisioned"
+  type        = string
+  default     = "us-east"
+  validation {
+    error_message = "Region must be in a supported IBM VPC region."
+    condition     = contains(["us-south", "us-east", "br-sao", "ca-tor", "eu-gb", "eu-de", "jp-tok", "jp-osa", "au-syd"], var.vpc_a_region)
+  }
+}
+
+variable "mgmt_vpc_zone" {
+  description = "The zone we are going to use for the prefix and subnet"
+  default = "us-east-1"  
+}
+
+variable "vpc_mgmt_prefix" {
+  description = "The CIDR block to use for the prefix"
+  default = "192.168.1.0/24"
+}
+
+variable "vpc_mgmt_sn" {
+  description = "The CIDR block to use for the subnet, should be the same as vpc_mgmt_prefix or a subnet within it"
+  default = "192.168.1.0/24"
+}
+
+variable "jumpserver_name" {
+  description = "The name of the jumpserver VSI"
+  default = "mgmt-jumpserver"
+}
+
+variable "jumpserver_image_id" {
+  description = "The image id for the OS to deploy on the jumpserver VSI"
+  default = "r014-17640494-4779-460c-99b2-000ff509896d"
+}
+
+variable "jumpserver_profile" {
+  description = "The server profile to deploy the jumpserver on"
+  default = "cx2-2x4"
 }
